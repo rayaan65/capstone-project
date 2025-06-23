ed78895e081cb416cd8dda8320fa256da00baae5
rayaan65 <rayyananwar65@gmail.com>
2025-06-23 21:15:59 +0530

    Merged latest changes

--- setup.py
+++ setup.py
@@ -5,23 +5,24 @@ setup(
     version="1.0.0",
     description="A data analysis and visualization web application",
     author="InsightFlow Team",
-    author_email="info@insightflow.com",
+    author_email="official.insightflow@gmail.com",
+    url="https://github.com/rayaan65/capstone-project.git",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "Flask>=3.1.0",
         "Werkzeug>=3.1.3",
-        "pandas>=2.2.0",
+        "pandas>=2.0.0",
         "numpy>=2.2.0",
         "matplotlib>=3.0.0",
         "seaborn>=0.12.0",
         "openpyxl>=3.0.0",
-        "xlrd>=2.0.0",
+        "xlrd>=2.0.0"
     ],
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-) 
+    ]
+)
